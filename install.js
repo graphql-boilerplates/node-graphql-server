a87adbf5e4c24e63ea8af01d1bc742c724d533f3
Johannes Schickling <schickling.j@gmail.com>
2017-12-09 17:09:31 +0100

    better install script

--- install.js
+++ install.js
@@ -1,11 +1,17 @@
 const fs = require('fs')
-const path = require('path')
 
 module.exports = ({ project }) => {
   const templateName = 'graphql-boilerplate'
   replaceInFile('package.json', templateName, project)
   replaceInFile('graphcool.yml', templateName, project)
   replaceInFile('.env', templateName, project)
+
+  console.log(`\
+Next steps:
+
+  1. Deploy database service: \`gc-db deploy\`
+  2. Start local server: \`yarn start\`
+  `)
 }
 
 function replaceInFile(filePath, searchValue, replaceValue) {
