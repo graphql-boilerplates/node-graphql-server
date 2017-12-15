877107a3e2a0d0ee32eae2d423f4507a83fecc40
Johannes Schickling <schickling.j@gmail.com>
2017-12-15 18:13:54 +0100

    init

---
+++ node-minimal/src/index.js
@@ -0,0 +1,46 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { importSchema } = require('graphql-import')
+const { Graphcool } = require('graphcool-binding')
+
+const typeDefs = importSchema('./src/schema.graphql')
+const resolvers = {
+  Query: {
+    feed(parent, args, ctx, info) {
+      return ctx.db.query.posts({ where: { isPublished: true } }, info)
+    },
+  },
+  Mutation: {
+    createDraft(parent, { title, text }, ctx, info) {
+      return ctx.db.mutation.createPost(
+        // TODO remove `isPublished` in favour of default value
+        { data: { title, text, isPublished: false } },
+        info,
+      )
+    },
+    publish(parent, { id }, ctx, info) {
+      return ctx.db.mutation.updatePost(
+        {
+          where: { id },
+          data: { isPublished: true },
+        },
+        info,
+      )
+    },
+  },
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Graphcool({
+      schemaPath: './database/schema.graphql',
+      endpoint:
+        'https://database-beta.graph.cool/api/graphql-template-node/dev',
+      secret: 'your-graphcool-secret',
+    }),
+  }),
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
