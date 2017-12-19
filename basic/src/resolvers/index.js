98b4dcf0be4e840be787179fb896e54147788935
Johannes Schickling <schickling.j@gmail.com>
2017-12-19 18:36:18 +0100

    fixed scalars

--- basic/src/resolvers/index.js
+++ basic/src/resolvers/index.js
@@ -2,7 +2,7 @@ const { Query } = require('./Query')
 const { auth } = require('./Mutation/auth')
 const { post } = require('./Mutation/post')
 const { AuthPayload } = require('./AuthPayload')
-const { DateTime } = require('graphcool-binding')
+const { scalars } = require('graphcool-binding')
 
 module.exports = {
   Query,
@@ -11,5 +11,5 @@ module.exports = {
     ...post,
   },
   AuthPayload,
-  DateTime,
+  DateTime: scalars.DateTime,
 }
