7244efc60d2d0ac2cece132510acb647f620dac9
Nikolas <nikolas.burk@gmail.com>
2017-12-20 19:50:41 +0100

    Update index.js

--- advanced/src/resolvers/index.js
+++ advanced/src/resolvers/index.js
@@ -2,7 +2,6 @@ const { Query } = require('./Query')
 const { auth } = require('./Mutation/auth')
 const { post } = require('./Mutation/post')
 const { AuthPayload } = require('./AuthPayload')
-const { scalars } = require('graphcool-binding')
 
 module.exports = {
   Query,
@@ -10,6 +9,5 @@ module.exports = {
     ...auth,
     ...post,
   },
-  AuthPayload,
-  DateTime: scalars.DateTime,
+  AuthPayload
 }
