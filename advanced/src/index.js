43c07ac053a39c62ede63fabb518673172f053be
Nilan Marktanner <nilan.marktanner@gmail.com>
2017-12-31 15:23:35 +0100

    upgraded to graphql-yoga@0.7.0

--- advanced/src/index.js
+++ advanced/src/index.js
@@ -1,17 +1,14 @@
 const { GraphQLServer } = require('graphql-yoga')
-const { importSchema } = require('graphql-import')
 const { Graphcool } = require('graphcool-binding')
 const resolvers = require('./resolvers')
 
-const typeDefs = importSchema('./src/schema.graphql')
-
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
   context: req => ({
     ...req,
     db: new Graphcool({
-      schemaPath: './database/schema.generated.graphql',
+      typeDefs: './database/schema.generated.graphql',
       endpoint: process.env.GRAPHCOOL_ENDPOINT,
       secret: process.env.GRAPHCOOL_SECRET,
     }),
