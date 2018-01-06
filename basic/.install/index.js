1a7d57e8a0af432501e64a188dd0dc2fc03f1bbb
Kim Brandwijk <kim.brandwijk@gmail.com>
2018-01-06 05:10:46 +0100

    feat(install): added graphcool deploy

--- basic/.install/index.js
+++ basic/.install/index.js
@@ -1,18 +1,21 @@
 const fs = require('fs')
+var spawn = require('cross-spawn')
 
-module.exports = ({ project }) => {
+module.exports = async ({ project }) => {
   const templateName = 'graphql-boilerplate'
+
   replaceInFile('src/index.js', templateName, project)
   replaceInFile('package.json', templateName, project)
   replaceInFile('graphcool.yml', templateName, project)
+
+  spawn.sync('./node_modules/.bin/graphcool', ['deploy'], { stdio: 'inherit'})
 
   console.log(`\
 Next steps:
 
   1. Change directory: \`cd ${project}\`
-  2. Deploy database service: \`graphcool deploy\`
-  3. Start local server: \`yarn start\`
-  4. Open Playground: http://localhost:4000
+  2. Start local server: \`yarn start\`
+  3. Open Playground: http://localhost:4000
   `)
 }
 
@@ -24,3 +27,5 @@ function replaceInFile(filePath, searchValue, replaceValue) {
   )
   fs.writeFileSync(filePath, newContents)
 }
+
+run()
