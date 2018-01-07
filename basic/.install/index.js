781b2d99145a47637ca0caf5a4c9a720412d0eed
Johannes Schickling <schickling.j@gmail.com>
2018-01-07 17:47:12 +0100

    use new install script

---
+++ basic/.install/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const {
+  replaceInFiles,
+  deploy,
+  writeEnv,
+  getInfo,
+} = require('graphql-boilerplate-install')
+
+module.exports = async ({ project }) => {
+  const templateName = 'graphql-boilerplate'
+
+  replaceInFiles(
+    ['src/index.js', 'package.json', 'database/graphcool.yml'],
+    templateName,
+    project,
+  )
+
+  console.log('Running $ graphcool deploy...')
+  await deploy(false)
+  const info = await getInfo()
+
+  replaceInFiles(['src/index.js'], '__GRAPHCOOL_ENDPOINT__', info.httpEndpoint)
+
+  console.log(`\
+Next steps:
+  1. Change directory: \`cd ${project}\`
+  2. Start local server: \`yarn start\`
+  3. Open Playground: http://localhost:4000
+`)
+}
