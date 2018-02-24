5e764048a5b60ab46db1cfd840edd54bbf05b2d1
Rex Raphael <rex.raphael@outlook.com>
2018-02-24 11:02:14 +0100

    Subscription feature added

---
+++ advanced/src/resolvers/Subscription.js
@@ -0,0 +1,14 @@
+const { getUserId } = require('../utils')
+
+const Subscription = {
+  async feedSubscription(parent, args, ctx, info) {
+    return await ctx.db.subscription.post({where: {
+      mutation_in: ['CREATED', 'DELETED', 'DELETED'],
+      node: {
+        isPublished: true
+      },
+    }})
+  },
+}
+
+module.exports = { Subscription }
