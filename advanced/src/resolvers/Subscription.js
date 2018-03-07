9408dd3f4a149c405909c9b5778dfa8deea80382
Nikolas <nikolas.burk@gmail.com>
2018-03-07 14:46:11 +0100

    Update Subscription.js

--- advanced/src/resolvers/Subscription.js
+++ advanced/src/resolvers/Subscription.js
@@ -1,14 +1,30 @@
-const { getUserId } = require('../utils')
-
 const Subscription = {
-  async feedSubscription(parent, args, ctx, info) {
-    return await ctx.db.subscription.post({ where: {
-      mutation_in: ['CREATED', 'DELETED', 'DELETED'],
-      node: {
-        isPublished: true
-      },
-    }}, info)
+  feedSubscription: {
+    subscribe: (parent, args, ctx, info) => {
+      return ctx.db.subscription.post({}, info)
+    },
   },
 }
+
+// This is the same subscription as above but only fires for 
+// posts that have been published.
+// This feature is not yet live due to a gub in Prisma:
+// https://github.com/graphcool/prisma/issues/1734
+// const Subscription = {
+//   feedSubscription: {
+//     subscribe: (parent, args, ctx, info) => {
+//       return ctx.db.subscription.post(
+//         {
+//           where: {
+//             node: {
+//               isPublished: true,
+//             },
+//           },
+//         },
+//         info,
+//       )
+//     },
+//   },
+// }
 
 module.exports = { Subscription }
