e88c4df359e2c4a69b871e8ff6ac374e28e3b08c
Johannes Schickling <schickling.j@gmail.com>
2018-04-26 11:56:09 +0200

    Update index.js

--- advanced/src/index.js
+++ advanced/src/index.js
@@ -2,18 +2,17 @@ const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 const resolvers = require('./resolvers')
 
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
+  endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma API (value set in `.env`)
+  debug: true, // log all GraphQL queries & mutations sent to the Prisma API
+  // secret: process.env.PRISMA_SECRET, // only needed if specified in `database/prisma.yml` (value set in `.env`)
+})
+
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: req => ({
-    ...req,
-    db: new Prisma({
-      typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
-      endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma API (value set in `.env`)
-      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
-      // secret: process.env.PRISMA_SECRET, // only needed if specified in `database/prisma.yml` (value set in `.env`)
-    }),
-  }),
+  context: req => ({ ...req, db }),
 })
 
 server.start(() => console.log('Server is running on http://localhost:4000'))
