ce355381746db1bbea680ebe40ed6aa628c00e3f
Nikolas Burk <nikolas.burk@gmail.com>
2018-06-07 16:07:30 +0200

    add proper subscriptions

--- advanced/src/resolvers/Subscription.js
+++ advanced/src/resolvers/Subscription.js
@@ -1,30 +1,18 @@
 const Subscription = {
   feedSubscription: {
     subscribe: (parent, args, ctx, info) => {
-      return ctx.db.subscription.post({}, info)
+      return ctx.db.subscription.post(
+        {
+          where: {
+            node: {
+              isPublished: true,
+            },
+          },
+        },
+        info,
+      )
     },
   },
 }
-
-// This is the same subscription as above but only fires for 
-// posts that have been published.
-// This feature is not yet live due to a bug in Prisma:
-// https://github.com/graphcool/prisma/issues/1734
-// const Subscription = {
-//   feedSubscription: {
-//     subscribe: (parent, args, ctx, info) => {
-//       return ctx.db.subscription.post(
-//         {
-//           where: {
-//             node: {
-//               isPublished: true,
-//             },
-//           },
-//         },
-//         info,
-//       )
-//     },
-//   },
-// }
 
 module.exports = { Subscription }
