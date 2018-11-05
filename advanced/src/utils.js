30c289070239c54a4aa3d67af64739f3f42ea200
siddhant1 <sidhant.manchanda@gmail.com>
2018-11-06 00:55:32 +0530

    Added flexibility to Auth

--- advanced/src/utils.js
+++ advanced/src/utils.js
@@ -1,23 +1,25 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-function getUserId(ctx) {
-  const Authorization = ctx.request.get('Authorization')
+function getUserId(ctx, requireAuth = true) {
+  const Authorization = ctx.request.get("Authorization");
   if (Authorization) {
-    const token = Authorization.replace('Bearer ', '')
-    const { userId } = jwt.verify(token, process.env.APP_SECRET)
-    return userId
+    const token = Authorization.replace("Bearer ", "");
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    return userId;
   }
-
-  throw new AuthError()
+  if (requireAuth) {
+    throw new AuthError();
+  }
+  return null;
 }
 
 class AuthError extends Error {
   constructor() {
-    super('Not authorized')
+    super("Not authorized");
   }
 }
 
 module.exports = {
   getUserId,
   AuthError
-}
+};
